125cb9f557eefba5b3ad85a032e41e4f74bf218d
MayaJRio <Mkamuren@gmail.com>
2021-06-12 21:31:46 +0300

    Created attendance migration

---
+++ database/migrations/2021_06_12_161530_create_attendances_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('totalhours')->default('0');
+            $table->tinyInteger('absentclasses')->default('0');
+            $table->tinyInteger('absenthours')->default('0');
+            $table->tinyInteger('percentabsent')->default('0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+
+}
