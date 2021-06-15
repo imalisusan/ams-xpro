8086229c168a3fe0672be81550437c26a57b960f
Imali Susan <susanimali52@gmail.com>
2021-06-15 10:17:26 -0700

    Chnage route name

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/studentprofile';
+    public const HOME = '/student/profile';
 
     /**
      * The controller namespace for the application.
