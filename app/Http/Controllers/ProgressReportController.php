eb2bb7d6d9a3b2df7e28b3b99e971e46e5234527
githubarj <85483196+githubarj@users.noreply.github.com>
2021-06-21 17:32:36 +0300

    Initial progress report feature

---
+++ app/Http/Controllers/ProgressReportController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+use Illuminate\Http\Request;
+use App\Models\CourseMark;
+use App\Models\CourseModule;
+use Illuminate\Support\Facades\Auth;
+
+
+class ProgressReportController extends Controller
+{
+    public function index(Request $request)
+
+    {
+        $courses = Course::all();
+        foreach ($courses as $course) 
+        {
+            $coursemodules = CourseModule::where('course_id', $course->id)->get();
+            $total = NULL;
+            if($coursemodules)
+                {
+                    foreach($coursemodules as $coursemodule)
+                    {
+                        $coursemark = CourseMark::where([
+                            ['course_module_id', '=', $coursemodule->id],
+                            ['user_id', '=', Auth::user()->id],
+                        ])->first();
+                        if($coursemark)
+                        {
+                            $coursemodule['score'] = $coursemark->score;
+
+                            $marks =  ($coursemodule['score'] * ( $coursemodule['weight'] * 100)) /  $coursemodule['maximum_score'];
+                            $total = $total + $marks;
+                            $total = number_format((float)$total, 2, '.', ''); 
+                        }
+                    
+                    }
+                }
+            
+        }
+        return view('progressreports.index', compact('courses'));
+        
+    }
+    
+}
