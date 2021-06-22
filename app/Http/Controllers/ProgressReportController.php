7792370a57d2ff0c8cb9219b88f892c3c48f518e
Imali Susan <35591292+imalisusan@users.noreply.github.com>
2021-06-22 15:37:32 +0300

    Update ProgressReportController.php

--- app/Http/Controllers/ProgressReportController.php
+++ app/Http/Controllers/ProgressReportController.php
@@ -28,20 +28,15 @@ class ProgressReportController extends Controller
                             ['course_module_id', '=', $coursemodule->id],
                             ['user_id', '=', Auth::user()->id],
                         ])->first();
-                        dd($coursemark);
                         if($coursemark)
                         {
                             $coursemodule['score'] = $coursemark->score;
                             $marks =  ($coursemodule['score'] * ( $coursemodule['weight'] * 100)) /  $coursemodule['maximum_score'];
                             $total = $total + $marks;
                             $course['total'] = number_format((float)$total, 2, '.', ''); 
-                            //$total = number_format((float)$total, 2, '.', ''); 
-                            
                         }
                     }
                 }
-            //dd($total);
-            //dd($course);
         }
         return view('progressreports.index', compact('courses'));
         
