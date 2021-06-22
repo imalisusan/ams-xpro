f9bf46b4af19ab5a2e3a51ae1690076bdffc63ac
Imali Susan <35591292+imalisusan@users.noreply.github.com>
2021-06-22 13:30:57 -0700

    Update pdf.blade.php

--- resources/views/progressreports/pdf.blade.php
+++ resources/views/progressreports/pdf.blade.php
@@ -2,7 +2,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('PROOGRESS REPORT') }} 
+            {{ __('Progress Report') }} 
         </h2>
     </x-slot>
 
