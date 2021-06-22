c58b299c89a7537f867cf186b0d1c66fb8b08cbe
Imali Susan <35591292+imalisusan@users.noreply.github.com>
2021-06-22 14:23:46 +0300

    Update index.blade.php

--- resources/views/progressreports/index.blade.php
+++ resources/views/progressreports/index.blade.php
@@ -51,10 +51,10 @@
                                      <tr>
                                         <td
                                         class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-black-500 tracking-wider">
-                                        {{ $course->name }}</td>
+                                        {{ $course->course->name }}</td>
                                         <td
                                         class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-black-500 tracking-wider">
-                                        {{ $course->year }}</td>
+                                        {{ $course->course->year }}</td>
                                     </tr>
 
                                      @endforeach
