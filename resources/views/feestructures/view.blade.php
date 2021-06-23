d10b523c9282339cf821a3570f1c0ef697c726f5
Rosy Waruku <rosy.waruku@strathmore.edu>
2021-06-23 23:50:14 +0300

    - Fee Strucutres CRUD -UI

--- resources/views/feestructures/view.blade.php
+++ resources/views/feestructures/view.blade.php
@@ -4,8 +4,8 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('View Fee Structure') }}&nbsp;&nbsp;
         </h2>
-        <div class="container container-fluid">
-        <iframe src ="{{ asset('/storage/feestructures/'.$feestructure->file_path) }}" class="responsive-iframe"width="1200px" height="600px"></iframe>
+        <div class="container-fluid">
+        <iframe src ="{{ asset('/storage/feestructures/'.$feestructure->file_path) }}" class="responsive-iframe" width="1200px" height="600px"></iframe>
         </div>
     </x-slot>
 </x-app-layout>
