562a1122f144279638dacd3372e56d3b43714dae
MayaJRio <Mkamuren@gmail.com>
2021-06-27 14:36:43 +0300

    Added attendance table

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -1,107 +1,48 @@
 <?php
 
-
 namespace App\Http\Controllers;
+
 use App\Models\Attendance;
+use App\Models\User;
 use App\Models\Course;
 use App\Models\CourseUser;
 use Illuminate\Http\Request;
-use App\Http\Requests\StoreAttendanceRequest;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreAttendanceRequest;
 
 class AttendanceController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index(Request $request)
     {
-        $attendance = Attendance::latest()->paginate(20);
-
-        return view('attendance.index', compact('attendance'))->with('i', (request()->input('page', 1) - 1) * 5);
+        $attendances = Attendance::all();
+        foreach($attendances as $attendance)
+        {
+            $attendance["absent_classes"] = 0.0;
+            $attendance["total_hours"] = 0.0;
+            $attendance["percentage_absent"] = 0.0;
+            $attendance["absent_hrs"] = 0.0;
+        }
+        $courses = Course::all();
+        //dd($attendances);
+        return view('attendance.index', compact('attendances', 'courses'))->with('i', (request()->input('page', 1) - 1) * 5);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+     
     public function create()
     {
-        return view('attendance.create');
+        $courseusers = CourseUser::all();
+        return view('attendance.create', compact('courseusers'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function register()
+    public function store(StoreAttendanceRequest $request)
     {
-        return view("attendance.register");
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreAttendanceRequest $request, CourseUser $course)
-    {
-
-        $validated = $request->input();
-        $user_id = Auth::user()->id;
-        $validated['user_id'] = $user_id;
+        $validated = $request->validated();
         Attendance::create($validated);
-
+     
         return redirect()->route('attendance.index')->with('success','Attendance created successfully.');
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function register()
     {
-        //
+        return view('attendance.register');
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update()
-    {
     
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
