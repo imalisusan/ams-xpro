ff290e1a531b9229152581ffeba98cf9af0717d4
Imali Susan <susanimali52@gmail.com>
2021-07-05 21:26:02 +0300

    Update navigation blade

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -30,16 +30,16 @@
                                     </button>
                                 </span>
                             </x-slot>
-
+                            
                             <x-slot name="content">
                                     <x-jet-dropdown-link  href="{{ route('courses.index') }}">
                                         {{ __('Self Registration') }}
                                     </x-jet-dropdown-link>
-
+                        
                                     <x-jet-dropdown-link href="{{ route('courses.personal') }}">
                                         {{ __('My Courses') }}
                                     </x-jet-dropdown-link>
-
+    
                                     <x-jet-dropdown-link href="{{ route('coursemarks.index') }}">
                                         {{ __('CourseWork Marks ') }}
                                     </x-jet-dropdown-link>
@@ -47,10 +47,10 @@
                                     <x-jet-dropdown-link href="{{ route('coursemodules.index') }}">
                                         {{ __('Course Modules') }}
                                     </x-jet-dropdown-link>
-
+                                    
                             </x-slot>
                         </x-jet-dropdown>
-                    </div>
+                    </div> 
                     <!-- End of Coursework Links -->
                     <!-- Fee Details Links -->
                     <div class="hidden sm:flex sm:items-center sm:ml-6">
@@ -79,37 +79,8 @@
                         </x-jet-dropdown>
                     </div> 
                     <!-- End of Fees Details Links -->
+
                     <x-jet-nav-link class="text-gray-900 text-base font-bold" href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                    <!-- Fees Details Links -->
-                    <div class="hidden sm:flex sm:items-center sm:ml-6">
-                        <x-jet-dropdown align="right" width="60">
-                            <x-slot name="trigger">
-                                <span class="inline-flex rounded-md">
-                                    <button type="button"
-                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                            Fees Details                                                                                                              
-                                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                            </svg>
-                                    </button>
-                                </span>
-                            </x-slot>
-                            
-                            <x-slot name="content">
-                                    <x-jet-dropdown-link href="{{ route('fees.feestatement') }}">
-                                        {{ __('Fees Statement') }}
-                                    </x-jet-dropdown-link>
-                        
-                                    <x-jet-dropdown-link href="{{ route('dashboard') }}">
-                                        {{ __('Fees Structure') }}
-                                    </x-jet-dropdown-link>    
-                            </x-slot>
-                        </x-jet-dropdown>
-                    </div> 
-                    <!-- End of Fees Details Links -->
-
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-
                         {{ __('Attendance') }}
                     </x-jet-nav-link>
                     <x-jet-nav-link class="text-gray-900 text-base font-bold" href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
@@ -119,7 +90,6 @@
                     <x-jet-nav-link class="text-gray-900 text-base font-bold" href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Exam Card') }}
                     </x-jet-nav-link>
-
                     @role('admin')
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Student Details') }}
@@ -130,7 +100,7 @@
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
-
+               
 
                 <!-- Settings Dropdown -->
                 <div class="ml-3 relative">
