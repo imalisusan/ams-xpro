9c95a1ec59ea043109264b530345efea239ff29b
Imali Susan <susanimali52@gmail.com>
2021-07-16 14:24:35 +0300

    Fix failing student problem

--- resources/views/courses/lecturer_coursemarks.blade.php
+++ resources/views/courses/lecturer_coursemarks.blade.php
@@ -1,8 +1,10 @@
  <!-- Lecturer Coursework Marks -->
-                    @php
-                    $coursemodules= $student->get_student_coursemarks($student->course_id, $student->user_id);
-                    $coursemark= $student->get_student_grademarks($student->course_id, $student->user_id)
-                    @endphp
+                   @foreach($students as $student)
+                        @php
+                        $coursemodules= $student->get_student_coursemarks($student->course_id, $student->user_id);
+                        $coursemark= $student->get_student_grademarks($student->course_id, $student->user_id)
+                        @endphp
+                   @endforeach
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 min-w-full">
                         <!-- component -->
                         <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8">
@@ -33,7 +35,10 @@
                                     @foreach($students as $student)
                                     <tbody class="bg-white"> 
                                  
-
+                                    @php
+                                    $coursemodules= $student->get_student_coursemarks($student->course_id, $student->user_id);
+                                    $coursemark= $student->get_student_grademarks($student->course_id, $student->user_id)
+                                    @endphp
                                     <td
                                         class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-black-500 tracking-wider">
                                         {{ $student->user->name }}</td>
