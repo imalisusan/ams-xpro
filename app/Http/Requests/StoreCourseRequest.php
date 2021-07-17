352a1a3071eb2c354cbb4a8560708c691113fa18
Imali Susan <susanimali52@gmail.com>
2021-07-17 13:14:06 +0300

    Add CRUD for course type

--- app/Http/Requests/StoreCourseRequest.php
+++ app/Http/Requests/StoreCourseRequest.php
@@ -31,6 +31,7 @@ class StoreCourseRequest extends FormRequest
             'credits' => 'required',
             'group' => 'required',
             'semester' => 'required',
+            'type' => 'required',
         ];
     }
 }
