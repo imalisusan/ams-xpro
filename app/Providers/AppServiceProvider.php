c64f815b8201b643d941a4c70a98d476841ec0fb
Imali Susan <35591292+imalisusan@users.noreply.github.com>
2021-08-08 19:22:58 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+         if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
