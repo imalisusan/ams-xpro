b9d34297febf23bde1c1489e2adc042513fe0510
Imali Susan <susanimali52@gmail.com>
2021-08-08 22:23:03 +0300

    Fix mentoring session index

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -72,6 +72,8 @@
                         </x-jet-dropdown>
                     </div> 
                     <!-- End of Coursework Links -->
+
+                    @role('student')
                     <!-- Fee Details Links -->
                     <div class="hidden sm:flex sm:items-center sm:ml-6">
                         <x-jet-dropdown>
@@ -98,12 +100,43 @@
                             </x-slot>
                         </x-jet-dropdown>
                     </div> 
+                    @endrole
                     <!-- End of Fees Details Links -->
 
+                    @role('admin')
+                    <!-- Fee Details Links -->
+                    <div class="hidden sm:flex sm:items-center sm:ml-6">
+                        <x-jet-dropdown>
+                            <x-slot name="trigger">
+                                <span class="inline-flex rounded-md">
+                                    <button type="button"
+                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-white transition ease-in-out duration-150">
+                                            Fees Details                                                                                                              
+                                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                            </svg>
+                                    </button>
+                                </span>
+                            </x-slot>
+                            
+                            <x-slot name="content">
+                                    <x-jet-dropdown-link href="{{ route('fees.feestatement')}}">
+                                        {{ __('Fee Statement') }}
+                                    </x-jet-dropdown-link>
+
+                                    <x-jet-dropdown-link href="{{ route('feestructures.index')}}" >
+                                        {{ __('Fee Structure') }}
+                                    </x-jet-dropdown-link>      
+                            </x-slot>
+                        </x-jet-dropdown>
+                    </div> 
+                    @endrole
+                    <!-- End of Fees Details Links -->
+                    @role('student')
                     <x-jet-nav-link class="text-gray-700 text-sm font-small" href="{{ route('attendance.index') }}" :active="request()->routeIs('attendance.index')">
                         {{ __('Attendance') }}
                     </x-jet-nav-link>
-                    @role('student')
+                    
                     <x-jet-nav-link class="text-gray-700 text-sm font-small" href="{{ route('student.progress') }}" :active="request()->routeIs('student.progress')">
                         {{ __('Progress Report') }}
                     </x-jet-nav-link>
